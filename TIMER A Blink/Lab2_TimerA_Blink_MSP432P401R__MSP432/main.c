a799b4ecb0261eb7af347c875c5279715fc68d00
albericoj3 <36242632+albericoj3@users.noreply.github.com>
2018-09-27 21:00:58 -0400

    Update main.c

--- TIMER A Blink/Lab2_TimerA_Blink_MSP432P401R__MSP432/main.c
+++ TIMER A Blink/Lab2_TimerA_Blink_MSP432P401R__MSP432/main.c
@@ -14,13 +14,13 @@ int main(void)
 	P2DIR |= BIT0; //sets port 2.0 as an output
 	//TA0CCTL1 |= CCIE;
 	TIMER_A0->CCTL[0] = TIMER_A_CCTLN_CCIE; // TACCR0 interrupt enabled
-    TIMER_A0->CCR[0] = 50000;
+        TIMER_A0->CCR[0] = 50000;
 	TIMER_A0->CCTL[1] = TIMER_A_CCTLN_CCIE; // TACCR1 interrupt enabled
 	TA0CCR0 = 50000;
 	TA0CCR1 = 25000;
 	TA0CTL = TASSEL_1 + MC_1 + TAIE; //ACLK up mode
 	__enable_irq();      // Enable global interrupt
-    NVIC->ISER[0] = 1 << ((TA0_0_IRQn) & 31);
+         NVIC->ISER[0] = 1 << ((TA0_0_IRQn) & 31);
 	while(1){
 
 	}
@@ -34,40 +34,17 @@ void TA0_0_IRQHandler(void) {
                 P1OUT ^= BIT0;
                 TA0CCR1 =50000;
                 break;
-
             case (4):
             case (6):
             case (8):
             case (10):
             case (12):
                 break;
-
             case(14):
                 P2OUT ^= BIT0;
                 TA0CCR1 = 25000;
                 break;
          }
 }
-
-/*#pragma vector=TIMER0_A1_VECTOR
-__interrupt void Timer_A (void)
-{
-    switch (TA0IV)
-    {
-        case(2):
-            P1OUT ^= BIT0;
-            TA0CCR1 =50000;
-            break;
-
-        case (4):
-            break;
-
-        case(10):
-            P1OUT ^= BIT6;
-            TA0CCR1 = 25000;
-            break;
-    }
-
-}*/
 
 
